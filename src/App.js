ef1f598f2e2edfc4abb4a2e6f3d4d744ae5d4507
mmmari4ka <109090925+mmmari4ka@users.noreply.github.com>
2023-03-31 18:21:51 +0200

    Added GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        Created by Ivanna Voitsekhovych, it is{" "}
+        <a
+          href="https://github.com/mmmari4ka/react-weather-app"
+          target="_blank"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub.
+      </footer>
+    </div>
+  );
 }
